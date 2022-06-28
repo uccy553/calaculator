f3d6590b1d4b37dd8b557186cda69884bf4b60f9
uccy553 <umonday98@gmail.com>
2022-06-28 18:57:57 -0400

    calculator

---
+++ index.js
@@ -0,0 +1,11 @@
+class calculator {
+ 
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll(['data-operation'])
+const equalsButton = document.querySelector(['data-equals'])
+const deleteButton = document.querySelector(['data-delete'])
+const alClearButton = document.querySelector(['data-all-clear'])
+const previousOperandandTextElement = document.querySelector(['data-previous-operand '])
+const previousOperandandTextElement = document.querySelector(['data-current-operand'])
